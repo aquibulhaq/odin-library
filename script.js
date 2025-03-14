0952b8a596a8f84c4096fb3a44a3fb569223742b
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-03-15 03:00:38 +0600

    Define Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Welcome to Odin Library!');
+function Book(title, author, pages, read) {
+  if (!new.target)
+    throw Error("You must use the 'new' operator to call the constructor");
+
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
