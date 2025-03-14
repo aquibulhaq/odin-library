40117fe39bf7443587eee4555d6681dcb31cc2c0
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-03-15 04:06:05 +0600

    Display library books in table

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const tbody = document.querySelector('tbody');
 
 function Book(title, author, pages, read) {
   if (!new.target)
@@ -17,5 +18,29 @@ function addBookToLibrary(title, author, pages, read) {
   return book;
 }
 
+function displayBookinTable(book) {
+  const tr = document.createElement('tr');
+
+  const titleTd = document.createElement('td');
+  titleTd.textContent = book['title'] ?? '';
+  tr.appendChild(titleTd);
+
+  const authorTd = document.createElement('td');
+  authorTd.textContent = book['author'] ?? '';
+  tr.appendChild(authorTd);
+
+  const pagesTd = document.createElement('td');
+  pagesTd.textContent = book['pages'] ?? 0;
+  tr.appendChild(pagesTd);
+
+  const readTd = document.createElement('td');
+  readTd.textContent = book['read'] ? 'Yes' : 'No';
+  tr.appendChild(readTd);
+
+  tbody.appendChild(tr);
+}
+
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary('Head First Design Patterns', 'Eric Freeman & Elisabeth Robson', 672, true);
+
+myLibrary.forEach(displayBookinTable);
