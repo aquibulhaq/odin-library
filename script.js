f9b15a2e6abc27bbb326bc07520f66ef716a5d60
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-03-15 03:01:55 +0600

    Define addBookToLibrary() function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   if (!new.target)
     throw Error("You must use the 'new' operator to call the constructor");
@@ -7,4 +9,10 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  return book;
 }
