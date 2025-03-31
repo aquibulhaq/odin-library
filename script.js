29675e0a7f3802c7b7056253d9298c012d0d1b41
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-03-31 23:07:01 +0600

    Wrap display logic inside DisplayController IIFE
    Also do the following:
     - Fix the typo displayBookinTable -> displayBookInTable.
     - Use dot notation to access Book fields.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const tbody = document.querySelector('tbody');
-
-const newBookBtn = document.querySelector('.new-book');
-
-const dialog = document.querySelector('dialog');
-
-const titleField = document.querySelector('#title');
-const authorField = document.querySelector('#author');
-const pagesField = document.querySelector('#pages');
-const readField = document.querySelector('#read');
-
-const cancelBtn = document.querySelector('.cancel');
-const submitBtn = document.querySelector('.submit');
-
 class Book {
   constructor(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -48,84 +34,98 @@ const library = (function Library() {
   };
 })();
 
-function displayBookinTable(book) {
-  const tr = document.createElement('tr');
-
-  const titleTd = document.createElement('td');
-  titleTd.textContent = book['title'] ?? '';
-  tr.appendChild(titleTd);
-
-  const authorTd = document.createElement('td');
-  authorTd.textContent = book['author'] ?? '';
-  tr.appendChild(authorTd);
-
-  const pagesTd = document.createElement('td');
-  pagesTd.textContent = book['pages'] ?? 0;
-  tr.appendChild(pagesTd);
-
-  const readTd = document.createElement('td');
-
-  const readChk = document.createElement('input');
-  readChk.type = 'checkbox';
-  readChk.checked = book['read'];
-  readChk.addEventListener('click', () => {
-    book.toggleRead();
-  });
-
-  readTd.appendChild(readChk);
-  tr.appendChild(readTd);
-
-  const removeTd = document.createElement('td');
-
-  const removeBtn = document.createElement('button');
-  removeBtn.classList.add('remove');
-  removeBtn.innerText = 'Remove Book';
-
-  const id = book.id;
-  removeBtn.addEventListener('click', () => {
-    tbody.removeChild(tr);
-
-    library.removeBookById(id);
-  });
-
-  removeTd.appendChild(removeBtn);
-  tr.appendChild(removeTd);
-
-  tbody.appendChild(tr);
-}
-
 library.addBook(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
 library.addBook(new Book('Head First Design Patterns', 'Eric Freeman & Elisabeth Robson', 672, true));
 
-library.books.forEach(displayBookinTable);
+(function DisplayController(library) {
+  const tbody = document.querySelector('tbody');
 
-newBookBtn.addEventListener('click', () => {
-  dialog.showModal();
-});
+  const newBookBtn = document.querySelector('.new-book');
 
-cancelBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  dialog.close();
-});
+  const dialog = document.querySelector('dialog');
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+  const titleField = document.querySelector('#title');
+  const authorField = document.querySelector('#author');
+  const pagesField = document.querySelector('#pages');
+  const readField = document.querySelector('#read');
 
-  const title = titleField.value;
-  const author = authorField.value;
-  const pages = Number(pagesField.value);
+  const cancelBtn = document.querySelector('.cancel');
+  const submitBtn = document.querySelector('.submit');
 
-  if (!title || !author || pages < 0 || !Number.isInteger(pages)) {
-    console.log(`Got invalid values: title="${title}", author="${author}", pages=${pages}`);
-    dialog.close();
-    return;
+  function displayBookInTable(book) {
+    const tr = document.createElement('tr');
+
+    const titleTd = document.createElement('td');
+    titleTd.textContent = book.title ?? '';
+    tr.appendChild(titleTd);
+
+    const authorTd = document.createElement('td');
+    authorTd.textContent = book.author ?? '';
+    tr.appendChild(authorTd);
+
+    const pagesTd = document.createElement('td');
+    pagesTd.textContent = book.pages ?? 0;
+    tr.appendChild(pagesTd);
+
+    const readTd = document.createElement('td');
+
+    const readChk = document.createElement('input');
+    readChk.type = 'checkbox';
+    readChk.checked = book.read;
+    readChk.addEventListener('click', () => book.toggleRead());
+
+    readTd.appendChild(readChk);
+    tr.appendChild(readTd);
+
+    const removeTd = document.createElement('td');
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove');
+    removeBtn.innerText = 'Remove Book';
+
+    const id = book.id;
+    removeBtn.addEventListener('click', () => {
+      tbody.removeChild(tr);
+
+      library.removeBookById(id);
+    });
+
+    removeTd.appendChild(removeBtn);
+    tr.appendChild(removeTd);
+
+    tbody.appendChild(tr);
   }
 
-  const read = readField.checked;
+  newBookBtn.addEventListener('click', () => {
+    dialog.showModal();
+  });
 
-  const book = new Book(title, author, pages, read);
-  library.addBook(book);
-  displayBookinTable(book);
+  cancelBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    dialog.close();
+  });
 
-  dialog.close();
-});
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const title = titleField.value;
+    const author = authorField.value;
+    const pages = Number(pagesField.value);
+
+    if (!title || !author || pages < 0 || !Number.isInteger(pages)) {
+      console.log(`Got invalid values: title="${title}", author="${author}", pages=${pages}`);
+      dialog.close();
+      return;
+    }
+
+    const read = readField.checked;
+
+    const book = new Book(title, author, pages, read);
+    library.addBook(book);
+    displayBookInTable(book);
+
+    dialog.close();
+  });
+
+  library.books.forEach(displayBookInTable);
+})(library);
